39d38f9c5cc5f2c7b3d4c17a1a712e38ce7b42ef
Erik Kallen <erikkallen@erikkallen.incas-3.local>
2011-01-05 11:59:33 +0100

    fix using module

--- lib/ultraviolet_syntax_patch.rb
+++ lib/ultraviolet_syntax_patch.rb
@@ -70,6 +70,7 @@ end
 # UV Syntax highlighting for Redmine
 #
 module UltravioletSyntaxPatch
+    require 'uv'
     class << self
       #def syntax_highlight_with_uv_syntax_highlight(name, content)
       def highlight_by_filename(content,name)#text, filename)
