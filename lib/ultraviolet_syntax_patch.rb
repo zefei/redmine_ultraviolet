af965fabf2496c77b4bc0fbac9d6e111a4d06417
Erik Kallen <erikkallen@erikkallen.incas-3.local>
2011-01-05 12:20:41 +0100

    fix using module

--- lib/ultraviolet_syntax_patch.rb
+++ lib/ultraviolet_syntax_patch.rb
@@ -83,7 +83,7 @@ module UltravioletSyntaxPatch
       
         ## User selection of UV Theme
         user_theme = User.current.custom_value_for(CustomField.first(:conditions => {:name => 'Ultraviolet Theme'}))
-        @uv_theme_name = user_theme || Uv::DEFAULT_THEME
+        @uv_theme_name = "amy"#user_theme || Uv::DEFAULT_THEME
 
         syntaxes = Uv.syntax_for_file(name, content)
 
